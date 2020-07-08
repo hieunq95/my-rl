59dd306bb6fc625af5941f9b071b45bd99769138
Hieu Nguyen <hieu1204@gmail.com>
2020-07-08 19:02:52 +0800

    update plot_learning

--- experiments/plot_learning.py
+++ experiments/plot_learning.py
@@ -2,8 +2,10 @@ import json
 import pandas
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.colors as mc
+import colorsys
 
-
+color_cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']  # colors used in plotting figures later
 """
 result_1
 nsteps_sarsa_result_1
@@ -17,25 +19,38 @@ f5 = './results/mountain_car_semi_gradient_sarsa/result.json'
 f6 = './results/mountain_car_q_learning/result_1.json'
 f7 = './results/mountain_car_dqn/mountain_car_dqn_1.json'
 f8 = './results/policy_gradient/reinforce.json'
-f = f8
+f = f6
 
 evaluated_metric = 'scores'
-window = 10
+window = 100
 metrics = ['reward', 'avg_reward', 'data_required', 'energy_required', 'latency_required', 'payment_required',
            'energy', 'latency', 'payment', 'data_1', 'data_2', 'actions', 'states']
 
 
+def adjust_lightness(color, amount=0.5):
+    try:
+        c = mc.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*mc.to_rgb(c))
+    return colorsys.hls_to_rgb(c[0], max(0, min(1, amount * c[1])), c[2])
+
+
 def plot_learning(file, ewm_window, metric):
     x_array, y_array, epsilons = [], [], []
+    y_bound = []
     with open(file) as json_file:
-        data = json.load(json_file)
-        ewm_data = pandas.read_json(file)[metric].ewm(span=ewm_window, adjust=False).mean()
+        data = pandas.read_json(json_file)
+        ewm_data = data[metric].ewm(span=ewm_window, adjust=False).mean()
         i = 0
         for p in data[metric]:
             x_array.append(i)
             y_array.append(ewm_data[i])
+            y_bound.append(p)
             i += 1
-        plt.plot(x_array, y_array, label=metric)
+
+        plt.plot(x_array, y_bound, label=evaluated_metric, color=adjust_lightness(color_cycle[0], 1.9))
+        plt.plot(x_array, y_array, label='mean-' + evaluated_metric, color=color_cycle[0])
         print('{}: {}'.format(metric, np.mean(y_array[-100:])))
         plt.legend()
         plt.show()
